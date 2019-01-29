bea4397177eb549c16e1bcd78f1a60d2fff4e5ef
Anuj Panchal <anujpan1@gmail.com>
2019-01-29 15:42:46 -0500

    add login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,13 @@ class UsersController < ApplicationController
   end
 
   post "/login" do
-    redirect "/users/:id"
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.username}"
+    else
+      redirect '/login'
+    end
   end
 
   # Sign Out
