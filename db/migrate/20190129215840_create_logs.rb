b7bf1107550354493c7e376ab1a4e031bd203907
Anuj Panchal <anujpan1@gmail.com>
2019-01-29 17:46:01 -0500

    add log migration and add form to create a new log

--- db/migrate/20190129215840_create_logs.rb
+++ db/migrate/20190129215840_create_logs.rb
@@ -1,7 +1,10 @@
 class CreateLogs < ActiveRecord::Migration
   def change
     create_table :logs do |t|
-      
+      t.string :date
+      t.string :start_time
+      t.string :end_time
+      t.integer :user_id
       t.timestamps null: false
     end
   end
